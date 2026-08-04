6a347af643371f90e0b9402d836c990ecc3e385e
Summer <summer@admin-03s-iMac.local>
2026-08-04 12:58:51 +0800

    tuesday

--- week05/tuesday/js/ex05.js
+++ week05/tuesday/js/ex05.js
@@ -1,12 +1,14 @@
 let currentX = 0;
 let circleY;
-function setup(){
+function setup() {
     createCanvas(600, 200);
     background(220);
     circleY = height / 2;
     noStroke();
 }
-function draw(){
+function draw() {
+    let g = map(currentX, 0, width, 0, 255);
+    fill(255, g, 0);
     circle(currentX, circleY, 50)
     currentX = currentX + 2
 }
